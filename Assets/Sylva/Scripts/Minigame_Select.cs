42dc403f376e05b48484fa1923f664c4c1dc70a7
Sylva <kierrific@gmail.com>
2026-01-29 12:14:43 -0800

    added a way to return to the main scene from my testing scenes

--- Assets/Sylva/Scripts/Minigame_Select.cs
+++ Assets/Sylva/Scripts/Minigame_Select.cs
@@ -6,11 +6,17 @@ using UnityEngine.SceneManagement;
 public class Minigame_Select : MonoBehaviour
 {
     [SerializeField] private List<string> minigameList = new();
+    [SerializeField] private string mainScene;
 
     public void Trigger()
     {
         int randomNumber = Random.Range(0, minigameList.Count);
 
         SceneManager.LoadScene(minigameList[randomNumber]);
+    }
+
+    public void ReturnScene()
+    {
+        SceneManager.LoadScene(mainScene);
     }
 }
