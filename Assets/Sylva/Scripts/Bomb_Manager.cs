c33881024e57db1b2baacfd40264de6b97cf4ff6
Sylva <kierrific@gmail.com>
2026-02-06 12:29:54 -0800

    finished bomb script to edit with player shenanigans

--- Assets/Sylva/Scripts/Bomb_Manager.cs
+++ Assets/Sylva/Scripts/Bomb_Manager.cs
@@ -16,8 +16,44 @@ public class Bomb_Manager : MonoBehaviour
     private bool startBomb;
     private GameObject bombObject;
 
+    //use this when a player collides with a different player, providing that one of them has the bomb
     public void Trigger(GameObject newPlayer)
     {
+        if (startBomb)
+        {
+            Destroy(bombObject);
+        }
         taggedPlayer = newPlayer;
+        bombObject = Instantiate(bombPrefab, taggedPlayer.transform.position, Quaternion.identity);
+    }
+
+    //preferably use this after a timer so the players can get ready for the minigame
+    public void StartBomb()
+    {
+        
+        Trigger(playerList[Random.Range(0, playerList.Count)]);
+        startBomb = true;
+    }
+
+    private void Update()
+    {
+        if (startBomb)
+        {
+            bombTime += Time.deltaTime;
+            if (bombTime >= bombTimer)
+            {
+                Debug.Log("kaboom"); //replace this with code that makes the bomb explode and eliminate the player.
+                Destroy(bombObject);
+            }
+        }
+    }
+
+    public void EndBomb()
+    {
+        if (bombObject != null)
+        {
+            Destroy(bombObject);
+        }
+        startBomb = false;
     }
 }
--- Assets/Sylva/Scripts/Bomb_Object.cs
+++ Assets/Sylva/Scripts/Bomb_Object.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class Bomb_Object : MonoBehaviour
 {
-    private GameObject playerObject;
+    
 }
