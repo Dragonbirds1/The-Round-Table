c9af15a7084f36fad816e0f5171d288e75041886
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2026-02-10 11:50:31 -0800

    no message

--- Assets/John/Scripts/Inventory.cs
+++ Assets/John/Scripts/Inventory.cs
@@ -6,28 +6,36 @@ using UnityEngine.InputSystem;
 public class Inventory : MonoBehaviour
 {
     public static List<itemData> items = new();
-    private bool _isOpen;
-    public GameObject inventory;
 
+    public bool _isOpen = false;
+
+    public GameObject inventory;
+ 
     public void OpenInventory()
     {
-        _isOpen = true;
-
+      
         inventory.SetActive(true);
+
         Time.timeScale = 0;
-        
+
     }
     public void CloseInventory()
     {
         _isOpen &= false;
-       Time.timeScale  =  1;
+
+        Time.timeScale = 1;
+
         inventory.SetActive(false);
     }
     public void ToggleInventory(InputAction.CallbackContext ctx)
     {
-      _isOpen = !_isOpen;
+       
+        if (ctx.canceled) return;
 
-        inventory.SetActive(!_isOpen);
+        _isOpen = !_isOpen;
+
+        inventory.SetActive(_isOpen);
+
         Time.timeScale = _isOpen ? 0 : 1;
     }
 }
